bea92ad07338fe454f90eb9069e716aded0c01fe
Vinnikovaannet <Vinnikova.anyta@yandex.ru>
2017-03-12 18:17:47 +0300

    Initial commit

---
+++ src/main/Point.java
@@ -0,0 +1,14 @@
+package main;
+
+class Point {
+    private double x, y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    boolean isSame(Point point) {
+        return (x == point.x && y == point.y);
+    }
+}
